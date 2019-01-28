d2be8f14f39dd48651664263d8aba4eda7df0961
linnea.gyllensvaan <linnea.gyllensvaan@elev.ga.lbs.se>
2019-01-28 09:20:17 +0100

    kuk

---
+++ Assets/GroundChecker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour {
+    public int isgrounded;
+    //om objektet nuddar triggern så händer nedanstående
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isgrounded ++; //kallar om du nuddar hitbox
+
+    }
+    //om objektet nuddar triggern  så händer nedanstående
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isgrounded --; //  kallar om du inte är på marken
+    }
+
+
+
+}
---
+++ Assets/movebitch.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movebitch : MonoBehaviour
+{
+
+
+    public float moveSpeed = 6f;
+    public float jumpspeed = 12f;
+    private Rigidbody2D rbody;
+    public GroundChecker groundchecker;
+
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+       
+    }
+
+    void Update()
+    {
+        rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rbody.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (groundchecker.isgrounded > 0)
+            {
+                rbody.velocity = new Vector2(rbody.velocity.x, jumpspeed);
+            }
+
+
+
+
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+
+    {
+        if (collision.tag == "Stege")
+        {
+            rbody.velocity = new Vector2(Input.GetAxis("Vertical") * moveSpeed, rbody.velocity.x);
+        }
+    }
+}
+
+
+  
+  
+            
+
+	
+	
